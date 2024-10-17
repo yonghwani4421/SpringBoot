41d2c385155ab14d1740dab3809638aa2e5c1bef
yonghwan <ghks4421@gmail.com>
2024-10-17 15:57:17 +0900

    commit

---
+++ Security/src/main/java/me/yonghwan/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -0,0 +1,17 @@
+package me.yonghwan.springbootdeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @AUTHOR dyd71
+ * @DATE 2024-10-17
+ * @PARAM
+ * @VERSION 1.0
+ */
+@SpringBootApplication
+public class SpringBootDeveloperApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDeveloperApplication.class, args);
+    }
+}
