c030db242c026392a1b88f416931faa02b07a7eb
yonghwan <ghks4421@gmail.com>
2024-10-17 14:51:58 +0900

    security

---
+++ Security/src/main/java/me/yonghwan/Main.java
@@ -0,0 +1,13 @@
+package me.yonghwan;
+
+/**
+ * @AUTHOR dyd71
+ * @DATE 2024-10-17
+ * @PARAM
+ * @VERSION 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
