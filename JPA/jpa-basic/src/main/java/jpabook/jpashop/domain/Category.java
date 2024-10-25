f80e69e44231a72fc555bc2238f673f646a313b5
김용환 <ghks4421@gmail.com>
2024-10-25 18:52:52 +0900

    지연로딩 적용 / CASCADE 적용

--- JPA/jpa-basic/src/main/java/jpabook/jpashop/domain/Category.java
+++ JPA/jpa-basic/src/main/java/jpabook/jpashop/domain/Category.java
@@ -5,6 +5,8 @@ import jakarta.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import static jakarta.persistence.FetchType.*;
+
 /**
  * @AUTHOR dyd71
  * @DATE 2024-10-24
@@ -18,7 +20,7 @@ public class Category extends BaseEntity{
     private Long id;
 
     private String name;
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "PARENT_ID")
     private Category parent;
 
