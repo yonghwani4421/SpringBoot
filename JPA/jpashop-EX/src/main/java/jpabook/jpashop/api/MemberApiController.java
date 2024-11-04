fbb3cdebc9c7c212a73c2d88750c6ec5abef745e
김용환 <ghks4421@gmail.com>
2024-11-04 21:18:17 +0900

    인증메일 구현 / 회원 api 구현

--- JPA/jpashop-EX/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ JPA/jpashop-EX/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.api;
 
 import jakarta.persistence.GeneratedValue;
 import jakarta.validation.Valid;
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
 import lombok.AllArgsConstructor;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,16 +23,37 @@ public class MemberApiController {
     public List<Member> membersV1(){
         return memberService.findMembers();
     }
+    @GetMapping("/api/v2/members")
+    public Result memberV2(){
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(collect);
+    }
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+    }
 
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member){
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
     }
-    @PostMapping("/api/v2/member")
+    @PostMapping("/api/v2/members")
     public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request){
         Member member = new Member();
         member.setName(request.getName());
+        member.setAddress(new Address(request.city,request.street,request.zipcode));
 
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
@@ -60,6 +83,9 @@ public class MemberApiController {
     @Data
     static class CreateMemberRequest{
         private String name;
+        private String city;
+        private String street;
+        private String zipcode;
     }
     @Data
     static class CreateMemberResponse{
