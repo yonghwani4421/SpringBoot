a9c91c199dc4b13d65dd1fe6bbae2631c400decc
김용환 <ghks4421@gmail.com>
2024-12-02 20:55:49 +0900

    query dsl 정렬 / 페이징 / 집합

--- JPA/query-dsl/src/test/java/study/query_dsl/QuerydslBasicTest.java
+++ JPA/query-dsl/src/test/java/study/query_dsl/QuerydslBasicTest.java
@@ -1,6 +1,7 @@
 package study.query_dsl;
 
 import com.querydsl.core.QueryResults;
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import org.assertj.core.api.Assertions;
@@ -12,12 +13,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.query_dsl.entity.Member;
 import study.query_dsl.entity.QMember;
+import study.query_dsl.entity.QTeam;
 import study.query_dsl.entity.Team;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 import static study.query_dsl.entity.QMember.member;
+import static study.query_dsl.entity.QTeam.*;
 
 @SpringBootTest
 @Transactional
@@ -121,7 +124,7 @@ public class QuerydslBasicTest {
     }
 
     @Test
-    @DisplayName("")
+    @DisplayName("기본 조회")
     public void resultFetch() throws Exception{
         // given
 //        List<Member> fetch = queryFactory.selectFrom(member)
@@ -144,7 +147,135 @@ public class QuerydslBasicTest {
         long total = queryFactory.selectFrom(member)
                 .fetchCount();
 
+    }
+
+    /**
+     * 회원 정렬 순서
+     * 1. 회원 나이 내림차순(desc)
+     * 2. 회원 이름 올림차순(asc)
+     * 단, 2에서 회원 이름이 없으면 마지막에 출력(nulls last)
+     * @throws Exception
+     */
+    @Test
+    @DisplayName("정렬")
+    public void sort() throws Exception{
+
+
+//        select m1_0.member_id,m1_0.age,m1_0.team_id,m1_0.username
+//        from member m1_0
+//        where m1_0.age=100
+//        order by m1_0.age desc,m1_0.username asc nulls last;
+        // given
+
+        em.persist(new Member(null, 100));
+        em.persist(new Member("member5", 100));
+        em.persist(new Member("member6", 100));
+
+
+        List<Member> result = queryFactory.selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(member.age.desc(), member.username.asc().nullsLast())
+                .fetch();
+
         // when
+        Member member5 = result.get(0);
+        Member member6 = result.get(1);
+        Member memberNull = result.get(2);
+
+        // then
+        assertThat(member5.getUsername()).isEqualTo("member5");
+        assertThat(member6.getUsername()).isEqualTo("member6");
+        assertThat(memberNull.getUsername()).isNull();
+    }
+
+    @Test
+    @DisplayName("페이징")
+    public void paging1() throws Exception{
+        // given
+
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .orderBy(member.username.desc())
+                .offset(1)
+                .limit(2)
+                .fetch();
+        // when
+
+        assertThat(result.size()).isEqualTo(2);
+        // then
+    }
+
+    @Test
+    @DisplayName("페이징")
+    public void paging2() throws Exception{
+        // given
+
+        QueryResults<Member> results = queryFactory
+                .selectFrom(member)
+                .orderBy(member.username.desc())
+                .offset(1)
+                .limit(2)
+                .fetchResults();
+        // when
+
+        assertThat(results.getTotal()).isEqualTo(4);
+        assertThat(results.getLimit()).isEqualTo(2);
+        assertThat(results.getOffset()).isEqualTo(1);
+        assertThat(results.getResults().size()).isEqualTo(2);
+        // then
+    }
+
+    @Test
+    @DisplayName("집합")
+    public void aggregation() throws Exception{
+        // given
+
+        List<Tuple> result = queryFactory
+                .select(
+                        member.count(),
+                        member.age.sum(),
+                        member.age.avg(),
+                        member.age.max(),
+                        member.age.min()
+                )
+                .from(member)
+                        .fetch();
+
+        Tuple tuple = result.get(0);
+        assertThat(tuple.get(member.count())).isEqualTo(4);
+        assertThat(tuple.get(member.age.sum())).isEqualTo(100);
+        assertThat(tuple.get(member.age.avg())).isEqualTo(25);
+        assertThat(tuple.get(member.age.max())).isEqualTo(40);
+        assertThat(tuple.get(member.age.min())).isEqualTo(10);
+
+        // when
+
+        // then
+    }
+
+    /**
+     * 팀의 이름과 각 팀의 평균 연령을 구해라
+     * @throws Exception
+     */
+    @Test
+    @DisplayName("group by")
+    public void group() throws Exception{
+        // given
+        List<Tuple> result = queryFactory
+                .select(team.name, member.age.avg())
+                .from(member)
+                .join(member.team, team)
+                .groupBy(team.name)
+                .fetch();
+        // when
+
+        Tuple teamA = result.get(0);
+        Tuple teamB = result.get(1);
+
+        assertThat(teamA.get(team.name)).isEqualTo("teamA");
+        assertThat(teamA.get(member.age.avg())).isEqualTo(15);
+        assertThat(teamB.get(team.name)).isEqualTo("teamB");
+        assertThat(teamB.get(member.age.avg())).isEqualTo(35);
 
         // then
     }
