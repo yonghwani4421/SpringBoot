2224ff6a7e5f27c5fd20ee7d7285ea3b6c59f3cd
김용환 <ghks4421@gmail.com>
2024-12-10 21:09:36 +0900

    query dsl 여러가지 동적쿼리 예제 / 수정 삭제 벌크연산 / sql function

--- JPA/query-dsl/src/test/java/study/query_dsl/QuerydslBasicTest.java
+++ JPA/query-dsl/src/test/java/study/query_dsl/QuerydslBasicTest.java
@@ -1,10 +1,13 @@
 package study.query_dsl;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.Expression;
 import com.querydsl.core.types.ExpressionUtils;
+import com.querydsl.core.types.Predicate;
 import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.JPAExpressions;
@@ -731,6 +734,175 @@ public class QuerydslBasicTest {
             System.out.println("memberDto = " + memberDto);
         }
 
+        // then
+    }
+
+    @Test
+    @DisplayName("dynamicQuery_BooleanBuilder")
+    public void dynamicQuery_BooleanBuilder() throws Exception{
+        // given
+        String usernameParam = "member1";
+        Integer ageParam = null;
+
+        List<Member> result = searchMember1(usernameParam, ageParam);
+        assertThat(result.size()).isEqualTo(1);
+        // when
+
+        // then
+    }
+
+    private List<Member> searchMember1(String usernameCond, Integer ageCond) {
+        BooleanBuilder builder = new BooleanBuilder();
+        if (usernameCond != null){
+            builder.and(member.username.eq(usernameCond));
+        }
+        if (ageCond != null) {
+            builder.and(member.age.eq(ageCond));
+        }
+        return queryFactory
+                .selectFrom(member)
+                .where(builder)
+                .fetch();
+    }
+
+    @Test
+    @DisplayName("dynamicQuery_WhereParam")
+    public void dynamicQuery_WhereParam() throws Exception{
+        // given
+        String usernameParam = "member1";
+        Integer ageParam = null;
+
+        List<Member> result = searchMember2(usernameParam, ageParam);
+        // when
+
+        // then
+        assertThat(result.size()).isEqualTo(1);
+    }
+
+    private List<Member> searchMember2(String usernameCond, Integer ageCond) {
+        return queryFactory
+                .selectFrom(member)
+//                .where(usernameEq(usernameCond), ageEq(ageCond))
+                .where(allEq(usernameCond,ageCond))
+                .fetch();
+    }
+
+    private BooleanExpression ageEq(Integer ageCond) {
+        return ageCond != null ? member.age.eq(ageCond) : null;
+    }
+
+    private BooleanExpression usernameEq(String usernameCond) {
+        return usernameCond != null ? member.username.eq(usernameCond) : null;
+    }
+    private BooleanExpression allEq(String usernameCond, Integer ageCond){
+        return usernameEq(usernameCond).and(ageEq(ageCond));
+    }
+
+    @Test
+    @DisplayName("updateBulk")
+    public void updateBulk() throws Exception{
+        // given
+        long count = queryFactory
+                .update(member)
+                .set(member.username, "비회원")
+                .where(member.age.lt(28))
+                .execute();
+        // when
+        assertThat(count).isEqualTo(2);
+        // then
+    }
+
+    @Test
+    @DisplayName("updateBulkAdd")
+    public void updateBulkAdd() throws Exception{
+        // given
+        long count = queryFactory
+                .update(member)
+                .set(member.age, member.age.add(1))
+                .execute();
+        // when
+
+        em.flush();
+        em.clear();
+        List<Member> result = queryFactory
+                .selectFrom(member).fetch();
+        // then
+        for (Member member1 : result) {
+            System.out.println("member1.age = " + member1.getAge());
+        }
+    }
+
+    @Test
+    @DisplayName("updateBulkMultiply")
+    public void updateBulkMultiply() throws Exception{
+        // given
+        long count = queryFactory
+                .update(member)
+                .set(member.age, member.age.multiply(2))
+                .execute();
+        // when
+
+        em.flush();
+        em.clear();
+        List<Member> result = queryFactory
+                .selectFrom(member).fetch();
+        // then
+        for (Member member1 : result) {
+            System.out.println("member1.age = " + member1.getAge());
+        }
+    }
+    @Test
+    @DisplayName("deleteBulk")
+    public void deleteBulk() throws Exception{
+        // given
+        long count = queryFactory
+                .delete(member)
+                        .where(member.age.gt(18))
+                                .execute();
+        // when
+        em.flush();
+        em.clear();
+        List<Member> result = queryFactory
+                .selectFrom(member).fetch();
+        // then
+        for (Member member1 : result) {
+            System.out.println("member1.age = " + member1.getAge());
+        }
+    }
+
+    @Test
+    @DisplayName("sqlFunction")
+    public void sqlFunction() throws Exception{
+        // given
+        List<String> result = queryFactory
+                .select(Expressions.stringTemplate(
+                        "function('replace', {0},{1},{2})"
+                        , member.username, "member", "M"))
+                .from(member)
+                .fetch();
+        // when
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+    }
+
+    @Test
+    @DisplayName("sqlFunction2")
+    public void sqlFunction2() throws Exception{
+        // given
+        List<String> result = queryFactory
+                .select(member.username)
+                .from(member)
+//                .where(member.username.eq(
+//                        Expressions.stringTemplate("function('lower',{0})", member.username))
+//                )
+                .where(member.username.eq(member.username.lower()))
+                .fetch();
+
+        // when
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
         // then
     }
 
